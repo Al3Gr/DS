762ed7df66142f277f259be52b9ce3dacfa04971
alessandrogravagno <alessandro.gravagno@hotmail.com>
2023-12-09 19:12:48 +0100

    prima implementazione delle route signup e login in gateway.py

--- gateway.py
+++ gateway.py
@@ -1,32 +1,48 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-#entrypoint registrazione utente
-@app.route("/signup")
-def user_signup():
-    #Comunicazione diretta gRPC con microUser per ottenere OKAY (TOKEN???)
-    pass
-
-#entrypoint login
-@app.route("/login")
-def user_login():
-    #Comunicazione diretta gRPC con microUser per ottenere OKAY (TOKEN???)
-    pass
+@app.route("/h<name>")
+def hello_world(name):
+    return f"<p>Hello, World {name}!</p>"
 
 
-#entrypoint post immagine
+@app.route("/test", methods=["GET"]) #testato passaggio di query parameters
+def test():
+    args = request.args
+    return args
+
+
+# entrypoint registrazione utente
+@app.post("/signup")
+def signup():
+    username = request.form["username"]
+    password = request.form["password"]
+    print(f"{username}:{password}")
+    # Comunicazione diretta gRPC con microUser per ottenere OKAY (TOKEN???)
+    return "Signup"
+
+
+# entrypoint login
+@app.post("/login")
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    print(f"{username}:{password}")
+    # Comunicazione diretta gRPC con microUser per ottenere OKAY (TOKEN???)
+    return "Login"
+
+
+# entrypoint post immagine
 @app.route("/post_image")
 def post_image():
-    #Comunicare con Kafka
+    # Comunicare con Kafka
     pass
 
-#entrypoint ottenere immagini
+
+# entrypoint ottenere immagini
 @app.route("/get_image")
 def get_image():
-    #Comunicare con Kafka
+    # Comunicare con Kafka
     pass
