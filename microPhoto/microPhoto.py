2a7ebfd3921d55a000e0a94c5f405ba1ef6e2de8
alessandrogravagno <alessandro.gravagno@hotmail.com>
2023-12-19 21:03:16 +0100

    implementato Token

--- microPhoto/microPhoto.py
+++ microPhoto/microPhoto.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 
 
 def check_token(func):
-    def decorator(*args, **kwargs):
+    def decorator():
         token = request.headers["Authorization"]
         decoded = jwt.decode(token, key= "segreto", algorithms="HS256")
         if decoded["valid"]:
@@ -34,8 +34,8 @@ def upload(username):
     #image = request.files // questo è come ottenere il file
     query = {"username": username, "description": description, "status": status}
     #image['image'].stream.read() // questo è il binary dell'immagine che bisogna mandare tramite grpc
-    photo_id = __db.addPhoto(query)
-    __db.updatePhoto(photo_id, PhotoState.UNTAGGED.name)
+    #photo_id = __db.addPhoto(query)
+    #__db.updatePhoto(photo_id, PhotoState.UNTAGGED.name)
     return "Test"
 
 
